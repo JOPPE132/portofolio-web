fce5ad60d532084c7337bda32e93a0e6f3946ce7
Mikkel <staveliem@gmail.com>
2023-09-14 13:05:17 +0200

    testing on phone to see if the hamburger menu works

--- src/header-contents/navigation.jsx
+++ src/header-contents/navigation.jsx
@@ -1,36 +1,70 @@
-import "./navigation.css"
-import {FaFacebook, FaGithub, FaLinkedin} from "react-icons/fa";
-
-//const ham = document.querySelector(".hamburger");
-//const nav = document.querySelector(".container-right");
-
-// ham.addEventListener("click", () => {
-//     ham.classList.toggle("active");
-//     nav.classList.toggle("active");
-// });
-//
-// document.querySelectorAll(".container-right").forEach(n =>
-//     n.addEventListener("click", () => {
-//         ham.classList.remove("active");
-//         nav.classList.remove("active");
-//     } ))
-
-const Socials = () => {
-    return(
-        <ul className="container-left">
-            <li><a href="https://github.com/JOPPE132" className="icons"><FaGithub/></a></li>
-            <li><a href="https://www.linkedin.com/in/mikkel-stavelie/" className="icons"><FaLinkedin/></a></li>
-            <li><a href="https://www.facebook.com/stavelie" className="icons"><FaFacebook/></a></li>
-        </ul>
-    )
-}
-
+import React, { useEffect } from 'react';
+import './navigation.css'
+import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 function Navigation() {
-    return(
+    useEffect(() => {
+        const ham = document.querySelector('.hamburger');
+        const nav = document.querySelector('.container-right');
+
+        // Add a click event listener to the hamburger icon
+        ham.addEventListener('click', () => {
+            ham.classList.toggle('active');
+            nav.classList.toggle('active');
+        });
+
+        // Add click event listeners to elements with the class "container-right"
+        document.querySelectorAll('.container-right').forEach((n) => {
+            n.addEventListener('click', () => {
+                ham.classList.remove('active');
+                nav.classList.remove('active');
+            });
+        });
+
+        return () => {
+            ham.removeEventListener('click', () => {
+                ham.classList.toggle('active');
+                nav.classList.toggle('active');
+            });
+
+            document.querySelectorAll('.container-right').forEach((n) => {
+                n.removeEventListener('click', () => {
+                    ham.classList.remove('active');
+                    nav.classList.remove('active');
+                });
+            });
+        };
+
+
+        // Clean up event listeners when the component unmounts
+    }, []); // The empty dependency array means this effect runs once when the component mounts
+
+    const Socials = () => {
+        return (
+            <ul className="container-left">
+                <li>
+                    <a href="https://github.com/JOPPE132" className="icons">
+                        <FaGithub />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/mikkel-stavelie/" className="icons">
+                        <FaLinkedin />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/stavelie" className="icons">
+                        <FaFacebook />
+                    </a>
+                </li>
+            </ul>
+        );
+    };
+
+    return (
         <header className="header-wrapper">
             <nav className="header-navigation">
-                <Socials/>
+                <Socials />
                 <ul className="container-right">
                     <li><a href="#">Hjem</a></li>
                     <li><a href="#about-wrapper">Om meg</a></li>
@@ -45,6 +79,7 @@ function Navigation() {
                 </div>
             </nav>
         </header>
-        )
+    );
 }
+
 export default Navigation;
