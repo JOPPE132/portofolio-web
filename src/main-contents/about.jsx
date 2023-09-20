996b819af08f4a5bba66bd1d5a89c9a9c16ed2a5
Mikkel <staveliem@gmail.com>
2023-09-20 16:53:17 +0200

    media queries for about me

--- src/main-contents/about.jsx
+++ src/main-contents/about.jsx
@@ -1,21 +1,6 @@
 import React from 'react'
 import './about.css'
 
-const abouttag = () => {
-    return(
-        <div>
-            <h1 className="about-title">Om meg</h1>
-            <p className="about-paragraph">Mitt navn er Mikkel Stavelie. Jeg er
-                (snart) en ferdigutdannet dataingeniør fra NTNU Ålesund som søker jobb som
-                fullstack/frontend-utvikler. Opprinnelig kommer jeg fra Trondheim. Jeg har røtter fra yrkesutdanning,
-                og ble ansatt som lærling hos GK Norge AS i 2017, hvor jeg tok fagbrevet som kuldemontør i 2019.
-                Sommeren 2021 ble jeg ansatt som butikkmedarbeider hos Rema 1000, og har vært deltidsansatt der gjennom hele
-                studietiden min på NTNU. Jeg tar med meg en rekke gode arbeidserfaringer og rutiner som gjelder
-                selvstendighet, nøyaktighet og samarbeid videre inn i arbeidslivet.</p>
-        </div>
-    )}
-
-
 export default function About(){
     return(
         <section id="about-wrapper">
